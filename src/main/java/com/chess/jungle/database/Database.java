fb3c0c4ba7bc7ebc9d82f2522bfc3badbd08bd89
Charles <1092637484@qq.com>
2021-01-13 19:17:27 +0800

    Create Database.java

---
+++ src/main/java/com/chess/jungle/database/Database.java
@@ -0,0 +1,9 @@
+package com.chess.jungle.database;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
